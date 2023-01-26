c295b1554be0b1f35760bc0e24271f34ba8c4fe2
alex-muehleisen <89556062+alex-muehleisen@users.noreply.github.com>
2023-01-26 18:38:09 +0900

    Upload Files

---
+++ scripts/check_web_endpoints.sh
@@ -0,0 +1,29 @@
+set -e
+
+host='localhost:1163'
+
+routes="
+/
+/ngrams
+/ngrams?query=trump
+"
+#/host
+#/host?host=nytimes.com
+#/metahtml
+#/metahtml?id=1
+
+failed=false
+for route in $routes; do
+    printf "[testing] $route \r"
+
+    if curl -s --fail "http://$host/$route" > /dev/null; then
+        echo '[pass]   '
+    else
+        echo '[fail]   '
+        failed=true
+    fi
+done
+
+if [ "$failed" = true ]; then
+    exit 1
+fi
